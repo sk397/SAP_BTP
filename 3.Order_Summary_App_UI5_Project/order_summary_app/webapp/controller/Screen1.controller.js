3ed80f9506ac422576fe4a44be82d96250762731
Sai Hari Krishnan <saikrishnan397@gmail.com>
2024-03-09 22:13:40 +0000

    UI5 Simple App, using Odata metadata

--- 3.Order_Summary_App_UI5_Project/order_summary_app/webapp/controller/Screen1.controller.js
+++ 3.Order_Summary_App_UI5_Project/order_summary_app/webapp/controller/Screen1.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/FilterType"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+
+
+    // The below is out order filter code
+    function (Controller, Filter, FilterOperator, FilterType) {
         "use strict";
 
         return Controller.extend("ordersummaryapp.controller.Screen1", {
-            onInit: function () {
+            onListPress:function(oEvent){
+            var orderID = oEvent.getParameter("listItem").getBindingContext().getProperty("OrderID");
 
-            }
+            var oFilter = new Filter("OrderID", FilterOperator.EQ, orderID);
+            this.getView().byId("orderTable").getBinding("items").filter(oFilter, FilterType.Application);
+          }    
         });
-    });
+ }
+    
+    );

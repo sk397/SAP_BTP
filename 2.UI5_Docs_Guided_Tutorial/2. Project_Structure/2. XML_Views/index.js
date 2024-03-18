584c46f9db6d18cb427deb99ece256864c2de5fa
Sai Hari Krishan <saikrishnan397@gmail.com>
2024-03-18 18:02:43 -0400

    Model View Controller (MVC) in UI5

---
+++ 2.UI5_Docs_Guided_Tutorial/2. Project_Structure/2. XML_Views/index.js
@@ -0,0 +1,14 @@
+// Again we define the dependecies and fucntion
+// Pass the XMLView Dependecy file and the XMLView module as a function 
+// the XMLCreate.view  reuturns a promise that we can use in the fcuniton
+
+
+sap.ui.define(['sap/ui/core/mvc/XMLView'], function(XMLView) {
+    'use strict';
+    
+    XMLView.create({
+        viewName: "ui5.product.list.view.App"
+    }).then(function(oView) {
+        oView.placeAt('content')
+    })
+});

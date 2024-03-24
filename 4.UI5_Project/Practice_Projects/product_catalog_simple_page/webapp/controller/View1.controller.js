b507649f831c16da12805228aac57e22163447fb
Sai Hari Krishan <saikrishnan397@gmail.com>
2024-03-23 22:33:28 -0400

    Added JavaScript Login to get the user input and delete items from list

--- 4.UI5_Project/Practice_Projects/product_catalog_simple_page/webapp/controller/View1.controller.js
+++ 4.UI5_Project/Practice_Projects/product_catalog_simple_page/webapp/controller/View1.controller.js
@@ -8,8 +8,32 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("productcatalogsimplepage.controller.View1", {
-            onInit: function () {
+            
+         // Getting the input from the user entered text when they click on the submit button 
+         // in the below fuction we are using this keyword to refer the view1 which is extended above
+         OnPressCreateNewProduct(){
+            const sProductName = this.getView().byId("UserProductEntry").getValue()
+            // console.log(sProductName);
+            const sCategory = this.getView().byId("CategoryID").getValue()
+            // console.log(sCategory);
+            const sReleaseDate = this.getView().byId("RelDate").getDateValue()
+            // console.log(sReleaseDate); - The Date is not looged in console check with Instructor
+            const sDiscontinuedDate = this.getView().byId("DisDate").getDateValue()
 
-            }
+         },
+
+
+         // Deleting a item from the list
+         OnDelete(random){
+            //Get the item from the list, we can pass any keyword to the function 
+            // This is the parameter that we get when we click on Delete icon [We can see this in the list  -> delete -> Events in Ui5 Docs]
+            const ItemFromList = random.getParameter("listItem")  
+            // console.log(ItemFromList.mProperties.title);
+            //Remove for list
+            this.getView().byId("ProductList").removeItem(ItemFromList)
+
+         }
+
         });
     });
+
